1112813ef1400f780faed807a242aaf19f17c606
andreastif <45339645+andreastif@users.noreply.github.com>
2024-03-09 16:51:43 +0100

    added http basic for proxy requests

--- src/main/java/com/rc/rcapi/configs/SecurityConfig.java
+++ src/main/java/com/rc/rcapi/configs/SecurityConfig.java
@@ -1,11 +1,53 @@
 package com.rc.rcapi.configs;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
 
+    @Value("${api.user}")
+    public String apiUsername;
 
-    //todo: add http basic for api calls
+    @Value("${api.pw}")
+    public String apiPw;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(withDefaults())
+                .formLogin(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req -> req.anyRequest().authenticated());
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails apiUser = User.builder()
+                .username(apiUsername)
+                .password(passwordEncoder().encode(apiPw))
+                .build();
+
+        return new InMemoryUserDetailsManager(apiUser);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
